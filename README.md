d7c97f0ddb56e996cd3e79c41e542aa152da9ef0
Vlad <starostenkovlad@gmail.com>
2022-08-27 14:44:32 +0300

    first-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Uber-clone mobile application written using React Native
+
+A live version of the application here: https://vladivals.github.io/uber-clone/#/
---
+++ components/NavFavourites.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import { useDispatch, useSelector } from "react-redux";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK",
+    coordinates: { lat: 51.5072178, lng: -0.1275862 },
+  },
+  {
+    id: "456",
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, london, UK",
+    coordinates: { lat: 51.51225220000001, lng: -0.1234514 },
+  },
+];
+
+const NavFavourites = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => (
+        <View style={[tw`bg-gray-200`, { height: 0.5 }]} />
+      )}
+      renderItem={({ item: { location, destination, icon, coordinates } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+          <View>
+            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+            <Text style={tw`text-gray-500`}>{destination}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavFavourites;
+
+const styles = StyleSheet.create({});
